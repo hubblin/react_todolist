427d8a547684e28b2a4e98fe20d1731762099842
hubblin <43435547+hubblin@users.noreply.github.com>
2021-07-07 22:34:06 +0900

    최적화 전 일정관리 완성

--- src/TodoInsert.js
+++ src/TodoInsert.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import './TodoInsert.css';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    },[])
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault();
+    },[onInsert, value])
+    
     return(
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할 일을 입력하세요"
+            value={value} onChange={onChange}/>
             <button type="submit">
                 <FontAwesomeIcon className="plusIcon" icon={['fas', 'plus']}/>
             </button>
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './TodoListItem.css';
+import cn from 'classnames';
 
-const TodoListItem = () => {
+const TodoListItem = ({todo,onRemove, onToggle}) => {
+    const {id,text, checked} = todo;
     return(
         <div className="TodoListItem">
-            <div className="checkbox">
-                <FontAwesomeIcon className="ch" icon={['far', 'square']}/>
-                <div className="text">할일</div>
+            <div className={cn('checkbox', {checked})} onClick={()=> onToggle(id)}>
+                {checked ? <FontAwesomeIcon className="ch" icon={['fas', 'check-square']} />:<FontAwesomeIcon className="ch" icon={['far', 'square']}/>}
+                <div className="text">{text}</div>
             </div>
-            <div className="remove">
-                <FontAwesomeIcon icon={['far', 'trash-alt']}/>
+            <div className="remove" onClick={()=>onRemove(id)}>
+                <FontAwesomeIcon icon={['far', 'trash-alt']} />
             </div>
         </div>
     )
